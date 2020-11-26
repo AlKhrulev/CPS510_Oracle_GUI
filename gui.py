2676e1cf52263429791089c6f2f76402b2f71f3c
AlKhrulev <alexkhrulev@protonmail.com>
2020-11-25 19:35:46 -0500

    added a method for 4 buttons(still bugged)

--- gui.py
+++ gui.py
@@ -5,11 +5,15 @@ def handle_connect_to_db(event):
     #write the code to connect to db here
     print("Successfully connected to the database")
 
-def handle_exit(event):
+def handle_exit(window,button1,button2):
     print("Exiting...")
-    exit(0)
+    update_window(window,button1,button2)
+    print("in handle exit")
 
 def create_window():
+    """
+    Create an initial window with 2 buttons
+    """
     window=tk.Tk()
     window.geometry("600x400") #widthxheight
     window.title("Database GUI Connector")
@@ -44,22 +48,40 @@ def create_window():
         height=5,
         bg="red",
         fg="black",
-        master=frame2
+        master=frame2,
+        command=handle_exit(window,button1,button2)
     )
     
     button2.pack()
     
     button1.bind("<Button-1>",handle_connect_to_db)
-    button2.bind("<Button-2>",handle_exit)
+    #button2.bind("<Button-2>",handle_exit)
 
-    return window
+    return window,button1,button2
 
-if __name__=="__main__":
-    window=create_window()
-    print(f"here")
-    #run an event loop
-    window.mainloop()
+def update_window(window,button1,button2):
+    """
+    Delete the previous 2 buttons and update the window
+    """
+    button1.destroy()
+    button2.destroy()
 
+    #the text to use for the buttons
+    new_text="CREATE tables,DROP tables,POPULATE tables,RUN a custom query".split(",")
+    new_text=[[new_text[0],new_text[1]],[new_text[2],new_text[3]]]
 
-    #must be present at the end
-    window.mainloop()
+    for i in range(2):
+        for j in range(2):
+            frame = tk.Frame(
+            master=window,
+            relief=tk.RAISED,
+            borderwidth=1)
+
+        frame.grid(row=i, column=j)
+        label = tk.Label(master=frame, text=new_text[i][j])
+        label.pack()
+
+window,button1,button2=create_window()
+print(f"here")
+#run an event loop
+window.mainloop()
