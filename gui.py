6f7add518d667940463fdda239c9b52bc5c96c52
kshmakov1 <62664259+kshmakov1@users.noreply.github.com>
2020-11-28 17:07:37 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import tkinter as tk
-from tkinter import N,S,W,E
+import cx_Oracle
+import config
+from tkinter import N,S,W,E, simpledialog
 
 class mainWindow():
     def __init__(self):
@@ -23,7 +25,8 @@ class mainWindow():
                           command=lambda: self.label.grid_forget())
         self.buttonBottomRight = tk.Button(self.root,
                           text = 'RUN A CUSTOM QUERY',
-                          command=lambda: self.label.grid())                     
+                          command=self.runCustomQuery)
+                          #command=lambda: self.label.grid())                     
        
         #for reference, don't delete yet padx=10, pady=10
         #
@@ -61,6 +64,14 @@ class mainWindow():
         pass
     def runCustomQuery(self):
         #TODO run a custiom query in a separate popup window
+        
+        query = simpledialog.askstring(title="Query", prompt=("Input Custom Query Here: " + " " * 100))
+        self.connection.execute(query)
+        
+        result = self.connection.fetchall()
+        
+        for x in result:
+            print(x)
         pass
 
     def connect_to_db(self):
@@ -69,7 +80,18 @@ class mainWindow():
         self.restore_layout()
         """
         #TODO paste the db connect code here
-
+        self.connection = None
+        try:
+            self.connection = cx_Oracle.connect(
+                config.username,
+                config.password,
+                config.dsn,
+                encoding=config.encoding)
+        
+            # show the version of the Oracle Database
+            print(self.connection.version)
+        except cx_Oracle.Error as error:
+            print(error)
         #restore the layout to the correct one
         self.restore_layout()
 
