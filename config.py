a23ccefd945b30f4bbcabe8c165ee787c59966f2
kshmakov1 <kshmakov@ryerson.ca>
2020-11-30 23:15:48 -0500

    current version

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-username = 'OT'
-password = '<password>'
-dsn = 'localhost/pdborcl'
+username = 'kshmakov'
+password = '10240293'
+dsn = 'oracle12c.scs.ryerson.ca/orcl12c'
 port = 1512
 encoding = 'UTF-8'
